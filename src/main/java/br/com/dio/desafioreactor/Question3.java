d007d02444a10b1dea5f8ca50ded1566d4e9be42
Maurício de Carvalho Lima <mauricio.lima@pucrs.br>
2025-03-19 21:29:30 -0300

    add questions

--- src/main/java/br/com/dio/desafioreactor/Question3.java
+++ src/main/java/br/com/dio/desafioreactor/Question3.java
@@ -8,6 +8,11 @@ public class Question3 {
     Verifica se o usuário passado é valido, caso seja retorna void, caso contrário deve disparar uma exception
     (para esse desafio vamos considerar que o usário é valido quando ele tem uma senha com mais de 8 caractéres)
      */
-    public Mono<Void> userIsValid(final User user){}
+    public Mono<Void> userIsValid(final User user){
+        return Mono.justOrEmpty(user)
+                .filter(u -> u.password() != null && u.password().length() > 8)
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("Senha inválida! A senha deve ter mais de 8 caracteres.")))
+                .then();
+    }
 
 }
